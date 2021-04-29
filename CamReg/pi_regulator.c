f8b19051850e299b415332066411e6178370ce6c
RobinJunod <82818451+RobinJunod@users.noreply.github.com>
2021-04-29 10:19:15 +0200

    color update

--- CamReg/pi_regulator.c
+++ CamReg/pi_regulator.c
@@ -1,3 +1,7 @@
+// must be renamed used for some test camera detection color
+
+
+
 #include "ch.h"
 #include "hal.h"
 #include <math.h>
@@ -11,35 +15,7 @@
 #include <process_image.h>
 
 //simple PI regulator implementation
-int16_t pi_regulator(float distance, float goal){
 
-	float error = 0;
-	float speed = 0;
-
-	static float sum_error = 0;
-
-	error = distance - goal;
-
-	//disables the PI regulator if the error is to small
-	//this avoids to always move as we cannot exactly be where we want and
-	//the camera is a bit noisy
-	if(fabs(error) < ERROR_THRESHOLD){
-		return 0;
-	}
-
-	sum_error += error;
-
-	//we set a maximum and a minimum for the sum to avoid an uncontrolled growth
-	if(sum_error > MAX_SUM_ERROR){
-		sum_error = MAX_SUM_ERROR;
-	}else if(sum_error < -MAX_SUM_ERROR){
-		sum_error = -MAX_SUM_ERROR;
-	}
-
-	speed = KP * error + KI * sum_error;
-
-    return (int16_t)speed;
-}
 
 static THD_WORKING_AREA(waPiRegulator, 256);
 static THD_FUNCTION(PiRegulator, arg) {
@@ -49,26 +25,39 @@ static THD_FUNCTION(PiRegulator, arg) {
 
     systime_t time;
 
+    char color = 'n';
     int16_t speed = 0;
-    int16_t speed_correction = 0;
+
 
     while(1){
         time = chVTGetSystemTime();
 
         //computes the speed to give to the motors
-        //distance_cm is modified by the image processing thread
-        speed = pi_regulator(get_distance_cm(), GOAL_DISTANCE);
-        //computes a correction factor to let the robot rotate to be in front of the line
-        speed_correction = (get_line_position() - (IMAGE_BUFFER_SIZE/2));
+        color = get_color();
 
-        //if the line is nearly in front of the camera, don't rotate
-        if(abs(speed_correction) < ROTATION_THRESHOLD){
-        	speed_correction = 0;
-        }
+
+       /* switch (color)
+        ​{
+            case 'r':
+              speed = -500;
+              break;
+
+            case 'n':
+              speed = 500;
+              break;
+
+            default:
+              // default statements
+        }*/
+
+        if (color == 'n')
+        	speed = 500;
+        if (color == 'r')
+        	speed = -500;
 
         //applies the speed from the PI regulator and the correction for the rotation
-		right_motor_set_speed(speed - ROTATION_COEFF * speed_correction);
-		left_motor_set_speed(speed + ROTATION_COEFF * speed_correction);
+		right_motor_set_speed(speed);
+		left_motor_set_speed(speed);
 
         //100Hz
         chThdSleepUntilWindowed(time, time + MS2ST(10));
